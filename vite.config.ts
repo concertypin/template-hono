09732b891c9efa359ae38a7f4fbd6cfd41b60c42
concertypin <55056558+concertypin@users.noreply.github.com>
2025-11-10 22:48:38 +0900

    No SSR

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
-import ssrPlugin from "vite-ssr-components/plugin";
 
 export default defineConfig({
-    plugins: [cloudflare(), ssrPlugin()],
+    plugins: [cloudflare()],
     server: {
         cors: false, // https://hono.dev/docs/middleware/builtin/cors#using-with-vite
     },
